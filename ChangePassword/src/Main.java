1e8ffc91dee05a002fb10d1bd5e4323fe939d783
4ex4ever4ex@gmail.com <4ex4ever4ex@gmail.com>
2024-04-16 13:40:39 +0500

    Поправил

--- ChangePassword/src/Main.java
+++ ChangePassword/src/Main.java
@@ -4,24 +4,20 @@ import exceptions.WrongPasswordException;
 public class Main {
 	public static void main(String[] args) {
 		String login = "Evgeniy_Chekhovich";
-		String password = "Passw0rd";
+		String password = "P@ssw0rd";
 		String confirmPassword = "Passw0rd";
-		if (changePassword(login, password, confirmPassword)) {
-			System.out.println("Пароль успешно обновлён");
-		};
+		changePassword(login, password, confirmPassword);
 	}
 
-	public static Boolean changePassword(String login, String password, String confirmPassword) {
+	public static void changePassword(String login, String password, String confirmPassword) {
 		try {
 			validateLogin(login);
 			validatePassword(password, confirmPassword);
-			return true;
 		} catch (WrongLoginException e) {
 			e.printStackTrace();
 		} catch (WrongPasswordException e) {
 			e.printStackTrace();
 		};
-		return false;
 	}
 
 	public static Boolean validateLogin(String login) throws WrongLoginException {
